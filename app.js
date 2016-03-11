cb772f050bcc90748d1df698c3439c4567bd5e0d
Frank Numrich <fnumrich@gmx.de>
2016-03-11 01:11:50 +0100

    Save image as image.jpg

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ angular.module('vehicleApp', [])
       mileageTo: 0
     }
     $scope.submit = function() {
-      $http.get('http://dc339.4shared.com/img/4UL0HmoW/s7/138d336b818/gambar-foto-lucu-bayi4')
+      $http.get('http://dc339.4shared.com/img/4UL0HmoW/s7/138d336b818/gambar-foto-lucu-bayi4', { responseType: 'arraybuffer' })
         .then(function(response) {
           console.log(response)
           var data = response.data
-          var blob = new Blob([data], { type: 'text/plain' })
-          var url = $window.URL || $window.webkitURL
-          $scope.fileUrl = url.createObjectURL(blob);
+          var blob = new Blob([data], { type: 'timage/jpeg' })
+          var a = document.createElement("a");
+          a.href = window.URL.createObjectURL(blob);
+          a.download = 'image.jpg';
+          a.click();
+          console.log(blob)
         })
     }
 
   }])
-  .config(['$compileProvider', function($compileProvider) {
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(|blob|):/);
-  }]);
